4edda10d9af3fa20257f7f1da288472e72ded810
Alexey-Eremin-lalala <149609252+Alexey-Eremin-lalala@users.noreply.github.com>
2025-12-16 02:19:11 +0500

    updated hiding error in project

--- src/scripts/components/validations.js
+++ src/scripts/components/validations.js
@@ -1,6 +1,16 @@
 const showInputError = (formElement,inputElement,errorMessage, errorClass,inputErrorClass) => {
     const errorElement = formElement.querySelector(`${inputElement.id}-error`);
-    inputElement.class.add(inputErrorClass);
+    inputElement.classList.add(inputErrorClass);
     errorElement.textContent = errorMessage;
     errorElement.classList.add(errorClass);
 } // если чето не так ошибка покажется
+
+const hideInputError = (formElement,inputElement, errorClass,inputErrorClass) => {
+    const errorElement = formElement.querySelector(`${inputElement.id}-error`);
+    inputElement.classList.remove(inputErrorClass);
+    errorElement.textContent='';
+    errorElement.classList.remove(errorClass);
+} // прятаем ошибочку
+
+
+// тут далее будет часть для ValidationSettings, которые указаны в чек-листе))
