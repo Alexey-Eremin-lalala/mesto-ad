573c299f5b518e01d3712d261d2af4cd7162803a
Alexey-Eremin-lalala <149609252+Alexey-Eremin-lalala@users.noreply.github.com>
2026-01-19 22:41:56 +0500

    carefully copied information from practicum

---
+++ src/scripts/components/api.js
@@ -0,0 +1,28 @@
+const config = {
+  baseUrl: "https://mesto.nomoreparties.co/v1/apf-cohort-202",
+  headers: {
+    authorization: "7fafe2b5-8740-449a-8849-7c52f79e98a8",
+    "Content-Type": "application/json",
+  },
+};
+
+const getResponseData = (res) => {
+  return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
+}; 
+
+export const getUserInfo = () => {
+  return fetch(`${config.baseUrl}/users/me`, { // Запрос к API-серверу
+    headers: config.headers, // Подставляем заголовки
+  }).then(getResponseData);  // Проверяем успешность выполнения запроса
+}; 
+
+export const setUserInfo = ({ name, about }) => {
+  return fetch(`${config.baseUrl}/users/me`, {
+    method: "PATCH",
+    headers: config.headers,
+    body: JSON.stringify({
+      name,
+      about,
+    }),
+  }).then(getResponseData);
+};
